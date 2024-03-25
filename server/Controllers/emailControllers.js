906480b076c0a83526fad61ba4d1420263adfa79
alphajames258 <james.lee4988@gmail.com>
2024-03-25 15:50:36 -0400

    Backend completed i think

---
+++ server/Controllers/emailControllers.js
@@ -0,0 +1,45 @@
+
+// import Emails from '../model/entryModel'
+const Emails = require('../model/entryModel.js')
+
+let emailControllers = {}
+
+emailControllers.postEmail = async (req, res, next) => {
+    try {
+        let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        const {email} = req.body
+        if (email.match(validRegex)) {
+        const newEmail = await Emails.create({email})
+        res.locals.email = newEmail
+        return next()
+    } else {
+        return next({message: 'not a valid email'})
+    }
+        
+        
+    } catch (error) {
+        return next({message: 'Earthquake'})
+        
+    }
+
+
+}
+emailControllers.deleteEmail = async(req, res, next) => {
+    try {
+        const {id} = req.params
+        if (id) {
+            const deleteEmail = await Emails.findByIdAndDelete(id)
+            return next()
+        } else {
+            return next({message: 'cant find id'})
+        }
+        
+    } catch (error) {
+        return next({message: "Earthquake"})
+        
+    }
+}
+
+
+
+module.exports = emailControllers
