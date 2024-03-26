ebff68b7051b7c9bde237eb280b8c6dc1c6a8814
punkygreg <guigui.v1999@gmail.com>
2024-03-25 20:07:59 -0400

    frontend tabs slightly broken

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,149 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  
+  // import Tabs from './lib/Tabs.svelte'
+  // import Router from "./lib/Router.svelte";
+  import Subscriber from "./lib/Subscriber.svelte";
+  import Docs from './lib/Docs.svelte';
+  import Roadmap from './lib/Roadmap.svelte';
+
+	// import Tab1 from "./Tab1.svelte";
+	// import Tab2 from "./Tab2.svelte";
+	// import Tab3 from "./Tab3.svelte";
+//   import Router from "./Router.svelte";
+
+  // List of tab items with labels, values and assigned components
+  let activeTabValue = 0;
+  let items = [
+    { label: "Docs",
+		 value: 1,
+		 component: Docs,
+		},
+    { label: "Roadmap",
+		 value: 2,
+		 component: Roadmap,
+		},
+    { label: "Subsribe To Newsletter",
+		 value: 3,
+		 component: Subscriber
+		}
+  ];
+
+
+
+  const handleClick = tabValue => () => (activeTabValue = tabValue);
 </script>
 
 <main>
+
+  <div >
+    <span class="header">
+      <h1>Sveltune</h1>
+      
+    </span>
+    <span id = "menu" class="header">
+      <!-- <Router {items}/> -->
+      <ul>
+    
+        {#each items as item}
+            <span  class={activeTabValue === item.value ? 'active' : ''}  on:click={handleClick(item.value)}>
+                {item.label}
+            </span>
+        {/each}
+        
+            <span><a class="link" href="https://github.com/oslabs-beta/Sveltune" target="_blank" rel="noreferrer">Medium</a></span>
+            <span><a class="link" href="https://github.com/oslabs-beta/Sveltune" target="_blank" rel="noreferrer">Github</a></span>
+            <span><a class="link" href="https://github.com/oslabs-beta/Sveltune" target="_blank" rel="noreferrer">Download</a></span>
+        </ul>
+    </span>
+  </div>
+
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    {#each items as item}
+        {#if activeTabValue == item.value}
+        <div class="box">
+            <svelte:component this={item.component}/>
+        </div>
+        {/if}
+    {/each}
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
+  <!-- <p>
+    Check out <a href="https://github.com/oslabs-beta/Sveltune" target="_blank" rel="noreferrer">Our Github</a>
+  </p> -->
+
+<!-- <span>
+  <Tabs/>
+</span> -->
+
+
+  <!-- <div class="card">
     <Counter />
-  </div>
+  </div> -->
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+ 
 
-  <p class="read-the-docs">
+  <!-- <p class="read-the-docs">
     Click on the Vite and Svelte logos to learn more
-  </p>
+  </p> -->
 </main>
 
 <style>
-  .logo {
+
+.header{
+  display: inline-block;
+
+}
+
+
+/* #tabs{
+        display: flex;
+        justify-content: flex-end;
+    } */
+
+    #link{
+        color: white;
+        transition: 0.25s ease-in;
+    }
+
+   span{
+    border: 1px solid transparent;
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
+    /* display: block; */
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    transition: 0.25s ease-in;
+    font-weight: 400;
+  }
+
+
+  span:hover, .link:hover {
+    /* border-color: #e9ecef #e9ecef #dee2e6; */
+    /* background-color: orangered; */
+    color: orangered;
+  }
+
+  .active {
+    color: orange;
+  }
+
+
+  /* .logo {
     height: 6em;
     padding: 1.5em;
     will-change: filter;
     transition: filter 300ms;
-  }
-  .logo:hover {
+  } */
+  /* .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
+  } */
+
+  /* .read-the-docs {
     color: #888;
-  }
+  } */
+
+
 </style>
